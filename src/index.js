ec54a4ebb4e633264913063a2b0e3a49167db4a3
Vitaly Ustsilouski <vitaliustsilouski@gmail.com>
2019-10-07 23:04:01 +0300

    resolve: task has been resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let includeOneOfBracketConfig;
+    if (str.length % 2 !== 0) {
+        return false;
+    } else {
+        bracketsConfig = bracketsConfig.map(element => element[0] + element[1]);
+        do {
+            includeOneOfBracketConfig = bracketsConfig.some(bracketsPair => {return str.includes(bracketsPair)});
+            bracketsConfig.forEach(bracketsPair => (str = str.replace(bracketsPair, '')));
+        } while (includeOneOfBracketConfig);
+        return !str;
+    }
 }
